df649be9c316a818a976e9f0d0193f5fabdc3c94
jeffliu <pengsi.liu@gmail.com>
2016-07-04 11:37:22 +0800

    rename mine nav bar title

--- CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/ui/activity/MineActivity.java
+++ CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/ui/activity/MineActivity.java
@@ -152,7 +152,7 @@ public class MineActivity extends AbstractActionBarActivity {
 
 		listAdapter.add(new MineItemsAdapter.PerformanceItem(String.format("本月积分 %s", performStat.get("totalMonthScore") == null ? "" : performStat.get("totalMonthScore")), -1, TYPE_TOTAL_SCORE, null));
 		listAdapter.add(new MineItemsAdapter.PerformanceItem(String.format("业绩 今日送%s单 打包%s 本月送%s单", performStat.get("myShipTotalD"), performStat.get("myPackageTotalD"), performStat.get("myShipTotal")), -1 /*Integer.parseInt(performStat.get("globalLateTotalD"))*/, TYPE_STORE_PERF, null));
-		listAdapter.add(new MineItemsAdapter.PerformanceItem("库存盘点", -1, TYPE_STORE_STORAGE, null));
+		listAdapter.add(new MineItemsAdapter.PerformanceItem("供货管理", -1, TYPE_STORE_STORAGE, null));
 
 		listAdapter.add(new MineItemsAdapter.PerformanceItem("客 户", -1, TYPE_USER_ITEMS, null));
 
