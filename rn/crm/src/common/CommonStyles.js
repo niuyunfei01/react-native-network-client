258bf2d0d5662f121647cd03765c5e5a2dd5bb8a
jeffliu <pengsi.liu@gmail.com>
2017-10-23 17:11:29 +0800

    customize cell styles for Height 70, Height 90

--- rn/crm/src/common/CommonStyles.js
+++ rn/crm/src/common/CommonStyles.js
@@ -9,7 +9,19 @@ const Styles = StyleSheet.create({
     borderColor: colors.color999,
     borderTopWidth: screen.onePixel,
     borderBottomWidth: screen.onePixel
-  }
+  },
+
+  cellTextH35: {
+    fontSize: 15,
+    marginTop: (35-20.5)/2,
+    marginBottom: (35-20.5)/2
+  },
+  
+  cellTextH45: {
+    fontSize: 16,
+    marginTop: (45-21.5)/2,
+    marginBottom: (45-21.5)/2
+  },
 })
 
 export default Styles
