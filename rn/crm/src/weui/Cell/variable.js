922d0a60463aa4cc6a02f6a6b257c9c6288b2c05
jeffliu <pengsi.liu@gmail.com>
2017-10-23 10:52:44 +0800

    support customized weui cells

--- rn/crm/src/weui/Cell/variable.js
+++ rn/crm/src/weui/Cell/variable.js
@@ -1,16 +1,16 @@
 export default {
   weuiCellBg: '#FFFFFF',
-  weuiCellBorderColor: '#D9D9D9',
+  weuiCellBorderColor: '#999999',//'#D9D9D9',
   weuiCellGapV: 0,
-  weuiCellGapH: 10,
+  weuiCellGapH: 15, //10
   weuiCellInnerGapH: 17 * 0.35, // '.35em'
-  weuiCellHeight: 42,
+  weuiCellHeight: 35, //42
   weuiCellFontSize: 16,
   weuiCellTipsFontSize: 14,
   weuiCellLabelWidth: 105,
 
   // unit((weuiCellHeight - 2 * weuiCellGapV) / weuiCellFontSize)
-  weuiCellLineHeight: 42,
+  weuiCellLineHeight: 35,  //42
   // unit(20 / @weuiCellFontSize, em),
   weuiCellsMarginTop: 10,
 }
