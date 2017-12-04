e09cc3a882904ccce6285c1be3b084f2055c46fd
jeffliu <pengsi.liu@gmail.com>
2017-12-04 12:52:55 +0800

    write release scripts

--- release_rn.sh
+++ release_rn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd rn/crm;
 react-native bundle --platform android --dev false --entry-file index.android.js \
 	--bundle-output ../../CainiaoCRM/src/main/assets/index.android.bundle \
 	--sourcemap-output ../../CainiaoCRM/src/main/assets/index.android.map \
 	--assets-dest ../../CainiaoCRM/src/main/res/
+cd -;
