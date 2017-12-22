2f0c7e951c637176e0e8da5960c0f8ed167d11b7
zhanghaobo <zhanghaobo@cainiaomeishi.com>
2017-12-22 20:09:01 +0800

    try to bugfix worker list

--- rn/crm/src/scene/User/UserScene.js
+++ rn/crm/src/scene/User/UserScene.js
@@ -309,7 +309,6 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     marginTop: pxToDp(30),
     marginBottom: pxToDp(25),
-    backgroundColor: '#f2f2f2',
   },
   user_name: {
     fontSize: pxToDp(30),
