7e9d5204703229bb1e758a167c11ecd9d429e618
shichaopeng <shichaopeng@ilegong.com>
2018-05-12 17:15:48 +0800

    bugfix

--- rn/crm/src/scene/Invoicing/InvoicingShippingScene.js
+++ rn/crm/src/scene/Invoicing/InvoicingShippingScene.js
@@ -95,13 +95,10 @@ class InvoicingShippingScene extends PureComponent {
         }} key={idx}>
           <CellHeader style={{justifyContent: 'center', height: pxToDp(180)}}>
             <Text style={[font.font30, font.fontBlack]}>{item['store_name']}</Text>
-            <Text
-              style={[font.font24, font.fontGray, {marginTop: pxToDp(10)}]}>{item['uid_confirm_name']} {item['time_confirm']}
-              提交</Text>
+            <Text style={[font.font24, font.fontGray, {marginTop: pxToDp(10)}]}>{item['uid_confirm_name']} {item['time_confirm']} 提交</Text>
             {!!item['remark'] ? <View style={{flexDirection: 'row', flexWrap:'nowrap'}}><Text
                 style={[font.font24, font.fontBlack, {marginTop: pxToDp(10)}]}>备注:</Text><Text
-                style={[font.font24, font.fontRed, {marginTop: pxToDp(11)}]} numberOfLines={1}
-                ellipsizeMode={'tail'}> {item['remark']}</Text></View> :
+                style={[font.font24, font.fontRed, {marginTop: pxToDp(11), flexWrap: 'wrap', width: pxToDp(450)}]}> {item['remark'].replace(/\s/g, ",")}</Text></View> :
               <Text style={[font.font24, {marginTop: pxToDp(10)}]}>无备注</Text>}
           </CellHeader>
           <CellBody/>
