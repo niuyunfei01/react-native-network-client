fad56bb6ba9b804cc44d3d530ce2c4ed083654d3
shichaopeng <shichaopeng@ilegong.com>
2018-05-17 16:18:18 +0800

    fix

--- release_rn.sh
+++ release_rn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd rn/crm;
 
-export BABEL_ENV=production
+
 
 react-native bundle --platform android --dev false --entry-file index.android.js \
 	--bundle-output ../../CainiaoCRM/src/main/assets/index.android.bundle \
 	--sourcemap-output ../../CainiaoCRM/src/main/assets/index.android.map \
-	--assets-dest ../../CainiaoCRM/src/main/res/ --reset-cache
+	--assets-dest ../../CainiaoCRM/src/main/res/
 
-export BABEL_ENV=dev
+
 
 cd -;
