538e7452549e0584eac4330a832ca934c62928cf
shichaopeng <shichaopeng@ilegong.com>
2018-06-20 14:51:51 +0800

    bugfix

--- CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/GlobalCtx.java
+++ CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/GlobalCtx.java
@@ -176,6 +176,7 @@ public class GlobalCtx extends Application {
                         return new HashMap<>();
                     }
                 });
+
     }
 
     @Nullable
--- CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/notify/service/AlwaysOnService.java
+++ CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/notify/service/AlwaysOnService.java
@@ -127,4 +127,5 @@ public class AlwaysOnService extends BaseService {
                     timeCount).commit();
         }
     }
+
 }
