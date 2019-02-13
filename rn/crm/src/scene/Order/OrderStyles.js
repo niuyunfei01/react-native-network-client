c2365ea50f6593a3a2b19fe22c47367070eed20c
fengtianhe <545704061@qq.com>
2019-02-13 12:34:18 +0800

    修改订单商品价格显示

--- rn/crm/src/scene/Order/OrderStyles.js
+++ rn/crm/src/scene/Order/OrderStyles.js
@@ -105,15 +105,24 @@ const styles = StyleSheet.create({
     paddingTop: 2,
     paddingBottom: 2
   },
-  product_img:{
+  product_img: {
     height: pxToDp(90),
     width: pxToDp(90),
-    marginRight:pxToDp(15),
-    borderRadius:10,
-    borderWidth:pxToDp(1),
-    borderColor:'#999'
+    marginRight: pxToDp(15),
+    borderRadius: 10,
+    borderWidth: pxToDp(1),
+    borderColor: '#999'
+  },
+  priceMode: {
+    borderWidth: pxToDp(1),
+    borderColor: '#ff6600',
+    color: '#ff6600',
+    marginRight: pxToDp(10),
+    height: pxToDp(25),
+    width: pxToDp(25),
+    fontSize: pxToDp(20),
+    textAlign: 'center'
   }
-
 });
 
 export default styles;
