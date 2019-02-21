df184d37c558690990cc87d458af50ecdc5f61f4
shichaopeng <shichaopeng@ilegong.com>
2019-02-21 10:13:50 +0800

    add ignore

--- release.sh
+++ release.sh
@@ -5,8 +5,7 @@ export BABEL_ENV=production
 ./release_rn.sh
 
 echo "build android apk..."
-#./gradlew clean assembleRelease
-./gradlew assembleRelease
+./gradlew clean assembleRelease
 
 export BABEL_ENV=dev
 
