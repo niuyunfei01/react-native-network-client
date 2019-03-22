47fc764b3ab4f35b730ad20164da4f6bdf1bd56f
fengtianhe <545704061@qq.com>
2019-03-22 15:58:31 +0800

    添加空页面

---
+++ rn/crm/src/scene/Inventory/ProductPutIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View} from "react-native";
+
+export default class ProductPutIn extends React.Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+      headerTitle: '商品入库'
+    }
+  }
+  
+  render () {
+    return (
+      <View></View>
+    );
+  }
+}
