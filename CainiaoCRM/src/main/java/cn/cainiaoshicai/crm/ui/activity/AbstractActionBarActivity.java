1dc1b12e6f478c51210129dfc18f9a56c9fc1114
jeffliu <pengsi.liu@gmail.com>
2019-04-23 20:35:55 +0800

    支持在所有activities中读取扫码

--- CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/ui/activity/AbstractActionBarActivity.java
+++ CainiaoCRM/src/main/java/cn/cainiaoshicai/crm/ui/activity/AbstractActionBarActivity.java
@@ -1,13 +1,23 @@
 package cn.cainiaoshicai.crm.ui.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import java.util.Map;
 
 import cn.cainiaoshicai.crm.GlobalCtx;
+import cn.cainiaoshicai.crm.scan.BluetoothScanGunKeyEventHelper;
+import cn.cainiaoshicai.crm.utils.BarCodeUtil;
 
 /**
  * Created by liuzhr on 6/1/16.
  */
-public class AbstractActionBarActivity extends AppCompatActivity {
+public class AbstractActionBarActivity extends AppCompatActivity implements BluetoothScanGunKeyEventHelper.OnScanSuccessListener {
+
+    private BluetoothScanGunKeyEventHelper mScanGunKeyEventHelper;
 
     @Override
     protected void onResume() {
@@ -20,6 +30,37 @@ public class AbstractActionBarActivity extends AppCompatActivity {
         super.onPause();
         if (GlobalCtx.app().getCurrentRunningActivity() == this) {
             GlobalCtx.app().setCurrentRunningActivity(null);
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mScanGunKeyEventHelper = new BluetoothScanGunKeyEventHelper(this);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (mScanGunKeyEventHelper.isScanGunEvent(event)) {
+            mScanGunKeyEventHelper.analysisKeyEvent(event);
+            return true;
+        }
+        return super.dispatchKeyEvent(event);
+//        mScanGunKeyEventHelper.analysisKeyEvent(event);
+//        return true;
+    }
+
+
+    @Override
+    public void onScanSuccess(String barcode) {
+        try {
+            Map<String, String> result = BarCodeUtil.extractCode(barcode);
+            GlobalCtx.app().scanInfo().add(result);
+            if(GlobalCtx.app().scanInfo().getLastTalking() - System.currentTimeMillis() > 1000){
+                GlobalCtx.app().toRnView(this, result.get("action"), result);
+            }
+        } catch (Exception e) {
+            System.out.println("scan code exception " + e.getMessage());
         }
     }
 }
