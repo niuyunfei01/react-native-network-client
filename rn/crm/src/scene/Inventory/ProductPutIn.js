264aa6a424b89f4b14c9c387b63773a64b25af9a
fengtianhe <545704061@qq.com>
2019-05-29 18:22:51 +0800

    fix bug

--- rn/crm/src/scene/Inventory/ProductPutIn.js
+++ rn/crm/src/scene/Inventory/ProductPutIn.js
@@ -77,9 +77,8 @@ class ProductPutIn extends React.Component {
       productId: navigation.state.params.pid,
       storeId: this.state.storeId
     }
-    Toast.loading('请求中', 0)
+    Toast.loading('请求中', 3)
     HttpUtils.post.bind(self.props)(api, data).then(res => {
-      Toast.hide()
       Toast.success('操作成功')
       native.nativeBack()
     })
