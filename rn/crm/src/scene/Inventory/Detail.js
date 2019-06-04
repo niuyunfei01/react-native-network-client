699cd2e4e1a09ae60c32052ea75f3273c01ce03c
fengtianhe <545704061@qq.com>
2019-06-04 17:47:17 +0800

    fix bug

--- rn/crm/src/scene/Inventory/Detail.js
+++ rn/crm/src/scene/Inventory/Detail.js
@@ -44,10 +44,10 @@ class Detail extends BaseComponent {
   
   fetchData () {
     const self = this
-    const {productId, storeId, page} = self.props.navigation.state.params
+    const {productId, storeId,} = self.props.navigation.state.params
     const uri = `/api_products/inventory_detail_history?access_token=${this.props.global.accessToken}`
     self.setState({isLoading: true})
-    HttpUtils.get.bind(self.props)(uri, {productId, storeId, page}).then(res => {
+    HttpUtils.get.bind(self.props)(uri, {productId, storeId, page: this.state.page}).then(res => {
       const lists = (this.state.page === 1 ? [] : this.state.lists).concat(res.lists)
       self.setState({isLastPage: res.isLastPage, lists: lists, isLoading: false, page: res.page + 1})
     })
