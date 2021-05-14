9c38bdcade02350d317250c5093374127fdb7cfb
liuzhoaren <liuzhaoren@cainiaomeishi.com>
2021-05-14 17:28:57 +0800

    add report exception to email

--- rn/crm/src/common/native.js
+++ rn/crm/src/common/native.js
@@ -208,10 +208,9 @@ export default {
       NativeModules.ActivityStarter.reportRoute(routeName))
   },
 
-  reportException: async function (msg, stack, currentExceptionID, isFatal) {
+  reportException: async function (msg) {
     console.log("error:", msg)
-    console.log("stack:", stack)
-    console.log("exceptionId:", currentExceptionID)
-    console.log("isFatal:", isFatal)
+    await (NativeModules.ActivityStarter &&
+      NativeModules.ActivityStarter.reportException(msg))
   }
 }
