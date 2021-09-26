795c88a256a05fe5398921d4ba184d789c255314
hwcncd <dingzhigang@cainiaomeishi.com>
2021-09-26 16:00:46 +0800

    增加工具函数Modal

--- rn/crm/src/util/ToastUtils.js
+++ rn/crm/src/util/ToastUtils.js
@@ -1,18 +1,40 @@
 'use strict';
-
-import {WToast} from 'react-native-smart-tip'
-//
-// const {
-//     ToastAndroid
-// } = ReactNative;
+import React from 'react';
+import {WModal, WToast} from 'react-native-smart-tip'
+import {ActivityIndicator, StyleSheet} from 'react-native'
+import {Icon} from "../weui";
 
 export function ToastShort(content) {
-
-  WToast.show({data: content, duration: 1000})
-  // ToastAndroid.show(content, ToastAndroid.SHORT);
+  WToast.show({data: content, duration: WToast.duration.SHORT})
 }
 
 export function ToastLong(content) {
-  WToast.show({data: content, duration: 2000})
-  // ToastAndroid.show(content, ToastAndroid.LONG);
+  WToast.show({data: content, duration: WToast.duration.LONG})
 }
+
+export function showModal(content, icon = 'loading') {
+  const modalOpts = {
+    data: content,
+    textColor: '#fff',
+    backgroundColor: '#444444',
+    position: WModal.position.CENTER,
+    icon: icon === 'loading' ? <ActivityIndicator color='#fff' size={'large'}/> :
+      <Icon name={icon} style={[styles.toastIcon]}/>
+  }
+
+  WModal.show(modalOpts)
+}
+
+export function hideModal() {
+  WModal.hide()
+}
+
+
+const styles = StyleSheet.create({
+  toastIcon: {
+    marginTop: 22,
+    color: '#fff',
+    fontSize: 55,
+    textAlign: 'center',
+  },
+})
