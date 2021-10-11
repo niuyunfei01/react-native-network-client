96b36353d9c602f486f509e7f81d1b996c0862af
niuyunfei01 <workweixin988ef0@cainiaomeishi.com>
2021-10-11 20:09:46 +0800

    处理了异常订单tab问题

--- rn/crm/src/scene/Order/OrderListScene.js
+++ rn/crm/src/scene/Order/OrderListScene.js
@@ -50,8 +50,8 @@ const labels = [];
 labels[Cts.ORDER_STATUS_TO_READY] = '待打包'
 labels[Cts.ORDER_STATUS_TO_SHIP] = '待配送'
 labels[Cts.ORDER_STATUS_SHIPPING] = '配送中'
-// labels[Cts.ORDER_STATUS_ABNORMAL] = '异常'
 labels[Cts.ORDER_STATUS_DONE] = '已完结'
+labels[Cts.ORDER_STATUS_ABNORMAL] = '异常'
 const initState = {
   canSwitch: true,
   isLoading: false,
@@ -118,7 +118,7 @@ class OrderListScene extends Component {
   }
 
   onTabClick = (tabData, index) => {
-    console.log("tab:", tabData, "index:", index)
+    // console.log("tab:", tabData, "index:", index)
     const query = this.state.query;
     if (query.listType !== tabData.type) {
       query.listType = tabData.type
@@ -131,7 +131,6 @@ class OrderListScene extends Component {
   }
 
   categoryTitles() {
-    // this.state.categoryLabels.splice(4,1,[Cts.ORDER_STATUS_ABNORMAL] = '异常' )
     return _.filter(this.state.categoryLabels.map((label, index) => {
       return {title: label, type: index}
     }), 'title')
@@ -165,9 +164,7 @@ class OrderListScene extends Component {
       const url = `/api/orders.json?access_token=${accessToken}`;
       const init = true;
       HttpUtils.get.bind(this.props)(url, params).then(res => {
-        // console.log('res!!!', res, res.params.status)
         const orderMaps = this.state.orderMaps;
-        // console.log('res.orders', res.orders)
         res.orders.map((item) => {
           if (item.is_right_once === true) {
             this.setState({
@@ -437,9 +434,14 @@ class OrderListScene extends Component {
 
   render() {
     let lists = [];
-
     this.state.categoryLabels.forEach((label, typeId) => {
-      const orders = this.state.orderMaps[typeId] || []
+      let tmpId = typeId;
+      if (typeId == 6){
+        tmpId = 8
+      }else if (typeId == 8){
+        tmpId = 6
+      }
+      const orders = this.state.orderMaps[tmpId] || []
       lists.push(
         <View
           key={`${typeId}`}
@@ -477,6 +479,7 @@ class OrderListScene extends Component {
                       justifyContent: 'space-evenly',
                       marginRight: -20,
                     }}>{
+                      [tabProps.tabs[3], tabProps.tabs[4]] = [tabProps.tabs[4], tabProps.tabs[3]],
                       tabProps.tabs.map((tab, i) => {
                         let total = this.state.totals[tab.type] || '0';
                         return <TouchableOpacity activeOpacity={0.9}
@@ -517,6 +520,7 @@ class OrderListScene extends Component {
                       }}
                       onEndReachedThreshold={0.5}
                       renderItem={this.renderItem}
+                      onRefresh={this.onRefresh.bind(this)}
                       refreshing={this.state.isLoading}
                       keyExtractor={this._keyExtractor}
                       shouldItemUpdate={this._shouldItemUpdate}
