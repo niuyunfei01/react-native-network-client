d079becb25710b4d70dc911b3328675901113360
hwcncd <dingzhigang@cainiaomeishi.com>
2021-11-17 14:04:47 +0800

    注册时将uid写入global

--- rn/crm/src/reducers/global/globalActions.js
+++ rn/crm/src/reducers/global/globalActions.js
@@ -416,14 +416,28 @@ export function updateStoresAutoDelivery(token, ext_store_id, params, callback) 
   }
 }
 
-export function customerApply(params, callback) {
+export function customerApply(params, callback, props) {
   return dispatch => {
     return addStores({device_uuid: getDeviceUUID(), ...params})
       .then((response) => {
         callback(true, '成功', response)
         const {access_token, refresh_token, expires_in: expires_in_ts} = response.user.token;
-        if (access_token) {
-          dispatch({type: SESSION_TOKEN_SUCCESS, payload: {access_token, refresh_token, expires_in_ts}});
+        dispatch({type: SESSION_TOKEN_SUCCESS, payload: {access_token, refresh_token, expires_in_ts}});
+        const expire = expires_in_ts || Config.ACCESS_TOKEN_EXPIRE_DEF_SECONDS;
+
+        const authCallback = (ok, msg, profile) => {
+          if (ok) {
+            dispatch(setUserProfile(profile));
+          }
+        };
+
+        if(Platform.OS ==='ios'){
+          doAuthLogin(access_token, expire, props , authCallback)
+        } else {
+          native.updateAfterTokenGot(access_token, expire, (ok, msg, strProfile) => {
+            const profile = ok ? JSON.parse(strProfile) : {};
+            authCallback(ok, msg, profile)
+          });
         }
       })
       .catch((error) => {
