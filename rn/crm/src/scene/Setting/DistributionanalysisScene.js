9e7799d4b3047217915f7e3e0ccf96d7a48575b6
hwcncd <dingzhigang@cainiaomeishi.com>
2021-11-23 11:16:29 +0800

    修复加载动画不关闭问题

--- rn/crm/src/scene/Setting/DistributionanalysisScene.js
+++ rn/crm/src/scene/Setting/DistributionanalysisScene.js
@@ -1,10 +1,5 @@
 import React, {PureComponent} from 'react'
-import {
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  Text, TouchableOpacity, View,
-} from 'react-native';
+import {RefreshControl, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
 import colors from "../../styles/colors";
 import pxToDp from "../../util/pxToDp";
 import {connect} from "react-redux";
@@ -59,8 +54,8 @@ class DistributionanalysisScene extends PureComponent {
   }
 
   componentDidMount() {
-    let startTime = Math.round(new Date(new Date().setHours(0, 0, 0, 0)).getTime()/1000)
-    let endTime = Math.round(new Date().getTime()/1000)
+    let startTime = Math.round(new Date(new Date().setHours(0, 0, 0, 0)).getTime() / 1000)
+    let endTime = Math.round(new Date().getTime() / 1000)
     this.getDistributionAnalysisData(startTime, endTime)
   }
 
@@ -74,33 +69,34 @@ class DistributionanalysisScene extends PureComponent {
     showModal("查询中");
     const api = `/v1/new_api/analysis/delivery/${currStoreId}?access_token=${accessToken}&starttime=${startTime}&endtime=${endTime}`
     HttpUtils.get.bind(this.props)(api).then((res) => {
+      this.setState({
+        DistributionanalysisData: res.data.statistic,
+        total_delivery: res.data.total_delivery,
+        total_fee: res.data.total_fee,
+      })
       hideModal()
-        this.setState({
-          DistributionanalysisData: res.data.statistic,
-          total_delivery: res.data.total_delivery,
-          total_fee: res.data.total_fee,
-        })
+
     })
   }
 
   setDateStatus(type) {
-      if(type == 3) {
-        this.setState({
-          showDateModal: true
-        })
-      }else{
-        let startTime
-        if (type == 0) {
-          startTime = Math.round(new Date(new Date().setHours(0, 0, 0, 0)).getTime()/1000)
-          }
-          if (type == 1) {
-            startTime = Math.round(new Date(new Date() - (new Date().getDay() + 6) * 86400000).setHours(0,0,0,0)/1000)
-          }
-          if (type == 2) {
-            startTime = Math.round(new Date(new Date().setDate(1)).setHours(0, 0, 0, 0)/1000)
-          }
-        let endTime = Math.round(new Date().getTime()/1000)
-        this.getDistributionAnalysisData(startTime,endTime)
+    if (type == 3) {
+      this.setState({
+        showDateModal: true
+      })
+    } else {
+      let startTime
+      if (type == 0) {
+        startTime = Math.round(new Date(new Date().setHours(0, 0, 0, 0)).getTime() / 1000)
+      }
+      if (type == 1) {
+        startTime = Math.round(new Date(new Date() - (new Date().getDay() + 6) * 86400000).setHours(0, 0, 0, 0) / 1000)
+      }
+      if (type == 2) {
+        startTime = Math.round(new Date(new Date().setDate(1)).setHours(0, 0, 0, 0) / 1000)
+      }
+      let endTime = Math.round(new Date().getTime() / 1000)
+      this.getDistributionAnalysisData(startTime, endTime)
     }
     this.setState({
       dateStatus: type,
@@ -130,7 +126,7 @@ class DistributionanalysisScene extends PureComponent {
     let targetDate = ''
     let dealTargetDays = ''
     const isLeapYear =
-        !!((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+      !!((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
     let countDays = 0
     for (let i = 0; i < many; i++) {
       if (flag === 'before') {
@@ -139,15 +135,15 @@ class DistributionanalysisScene extends PureComponent {
         month = month + 1 > 12 ? 1 : month + 1
       }
       thirtyDays.includes(month)
-          ? (countDays += 30)
-          : thirtyOneDays.includes(month)
-              ? (countDays += 31)
-              : isLeapYear
-                  ? (countDays += 29)
-                  : (countDays += 28)
+        ? (countDays += 30)
+        : thirtyOneDays.includes(month)
+          ? (countDays += 31)
+          : isLeapYear
+            ? (countDays += 29)
+            : (countDays += 28)
     }
     targetDateMilli = currDate.setDate(
-        currDate.getDate() - (flag === 'before' ? countDays : countDays * -1)
+      currDate.getDate() - (flag === 'before' ? countDays : countDays * -1)
     )
     GMTDate = new Date(targetDateMilli)
     targetYear = GMTDate.getFullYear()
@@ -162,92 +158,114 @@ class DistributionanalysisScene extends PureComponent {
   renderCellItem() {
     const {DistributionanalysisData, tabelTitle, total_delivery, total_fee} = this.state
     let Array = []
-    for(let i in DistributionanalysisData){
+    for (let i in DistributionanalysisData) {
       Array.push(DistributionanalysisData[i])
     }
     return (
-        <ScrollView
-            refreshControl={
-              <RefreshControl
-                  refreshing={this.state.isRefreshing}
-                  // onRefresh={() => this.onHeaderRefresh()}
-                  tintColor='gray'
-              />
-            }
-            style={{backgroundColor: colors.main_back}}
-        >
-          <View style={[styles.cell_box_header]}>
-            {this.state.dateStatus === 0 ? <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today]} onPress={() =>{this.setDateStatus(0)}}>今天</Text>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            // onRefresh={() => this.onHeaderRefresh()}
+            tintColor='gray'
+          />
+        }
+        style={{backgroundColor: colors.main_back}}
+      >
+        <View style={[styles.cell_box_header]}>
+          {this.state.dateStatus === 0 ?
+            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+              <Text style={[styles.cell_rowTitleText_today]} onPress={() => {
+                this.setDateStatus(0)
+              }}>今天</Text>
             </View> : <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today1]} onPress={() =>{this.setDateStatus(0)}}>今天</Text>
+              <Text style={[styles.cell_rowTitleText_today1]} onPress={() => {
+                this.setDateStatus(0)
+              }}>今天</Text>
             </View>}
-            {this.state.dateStatus === 1 ? <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today]} onPress={() =>{this.setDateStatus(1)}}>近7天</Text>
+          {this.state.dateStatus === 1 ?
+            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+              <Text style={[styles.cell_rowTitleText_today]} onPress={() => {
+                this.setDateStatus(1)
+              }}>近7天</Text>
             </View> : <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today1]} onPress={() =>{this.setDateStatus(1)}}>近7天</Text>
+              <Text style={[styles.cell_rowTitleText_today1]} onPress={() => {
+                this.setDateStatus(1)
+              }}>近7天</Text>
             </View>}
-            {this.state.dateStatus === 2 ? <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today]} onPress={() =>{this.setDateStatus(2)}}>本月</Text>
+          {this.state.dateStatus === 2 ?
+            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+              <Text style={[styles.cell_rowTitleText_today]} onPress={() => {
+                this.setDateStatus(2)
+              }}>本月</Text>
             </View> : <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today1]} onPress={() =>{this.setDateStatus(2)}}>本月</Text>
+              <Text style={[styles.cell_rowTitleText_today1]} onPress={() => {
+                this.setDateStatus(2)
+              }}>本月</Text>
             </View>}
-            {this.state.dateStatus === 3 ? <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today]} onPress={() =>{this.setDateStatus(3)}}>自定义</Text>
+          {this.state.dateStatus === 3 ?
+            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+              <Text style={[styles.cell_rowTitleText_today]} onPress={() => {
+                this.setDateStatus(3)
+              }}>自定义</Text>
             </View> : <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText_today1]} onPress={() =>{this.setDateStatus(3)}}>自定义</Text>
+              <Text style={[styles.cell_rowTitleText_today1]} onPress={() => {
+                this.setDateStatus(3)
+              }}>自定义</Text>
             </View>}
-          </View>
+        </View>
 
-          <View style={[styles.cell_box]}>
-            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText]}>配送费</Text>
-              <Text style={[styles.cell_rowTitleText1]}>¥{total_fee}</Text>
-            </View>
-            <View style={{width: pxToDp(2), height: pxToDp(120), backgroundColor: colors.colorDDD, marginTop: pxToDp(20)}}/>
-            <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
-              <Text style={[styles.cell_rowTitleText]}>总发单量</Text>
-              <Text style={[styles.cell_rowTitleText1]}>{total_delivery}单</Text>
-            </View>
+        <View style={[styles.cell_box]}>
+          <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+            <Text style={[styles.cell_rowTitleText]}>配送费</Text>
+            <Text style={[styles.cell_rowTitleText1]}>¥{total_fee}</Text>
           </View>
-          <View style={[styles.cell_box1]}>
-            <View style={{width: pxToDp(356), flexDirection: "row", marginTop: pxToDp(20), paddingLeft: pxToDp(10)}}>
-              { tabelTitle.map((item, index) => {
-                return (
-                  <View style={styles.box} key={index}>
-                    <Text style={styles.name1}>{item}</Text>
-                  </View>
-                )
-              }) }
-            </View>
-            {Array.map((item) => {
-              const avg_distance = ((item.avg_distance)/1000).toString()
-              let str = avg_distance.substring(0, 3)
+          <View
+            style={{width: pxToDp(2), height: pxToDp(120), backgroundColor: colors.colorDDD, marginTop: pxToDp(20)}}/>
+          <View style={{flexDirection: "column", marginVertical: pxToDp(20), alignItems: "center"}}>
+            <Text style={[styles.cell_rowTitleText]}>总发单量</Text>
+            <Text style={[styles.cell_rowTitleText1]}>{total_delivery}单</Text>
+          </View>
+        </View>
+        <View style={[styles.cell_box1]}>
+          <View style={{width: pxToDp(356), flexDirection: "row", marginTop: pxToDp(20), paddingLeft: pxToDp(10)}}>
+            {tabelTitle.map((item, index) => {
               return (
-                <View style={{width: pxToDp(356), flexDirection: "row", marginVertical: pxToDp(10), paddingLeft: pxToDp(10)}}>
-                  <View style={styles.box}>
-                    <Text style={styles.name}>{item.platform}</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.name}>{item.total_fee}元</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.name}>{item.total_count}</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.name}>{item.avg_fee}元</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.name}>{str}km</Text>
-                  </View>
+                <View style={styles.box} key={index}>
+                  <Text style={styles.name1}>{item}</Text>
                 </View>
               )
             })}
           </View>
-          <Dialog visible={this.state.showDateModal} onRequestClose={() => this.onRequestClose()}>
-            {this.showDatePicker()}
-          </Dialog>
-        </ScrollView>
+          {Array.map((item) => {
+            const avg_distance = ((item.avg_distance) / 1000).toString()
+            let str = avg_distance.substring(0, 3)
+            return (
+              <View
+                style={{width: pxToDp(356), flexDirection: "row", marginVertical: pxToDp(10), paddingLeft: pxToDp(10)}}>
+                <View style={styles.box}>
+                  <Text style={styles.name}>{item.platform}</Text>
+                </View>
+                <View>
+                  <Text style={styles.name}>{item.total_fee}元</Text>
+                </View>
+                <View>
+                  <Text style={styles.name}>{item.total_count}</Text>
+                </View>
+                <View>
+                  <Text style={styles.name}>{item.avg_fee}元</Text>
+                </View>
+                <View>
+                  <Text style={styles.name}>{str}km</Text>
+                </View>
+              </View>
+            )
+          })}
+        </View>
+        <Dialog visible={this.state.showDateModal} onRequestClose={() => this.onRequestClose()}>
+          {this.showDatePicker()}
+        </Dialog>
+      </ScrollView>
     )
   }
 
@@ -255,7 +273,8 @@ class DistributionanalysisScene extends PureComponent {
     const dealTargetDays = this.getNewDate('before', 3)
     return <View>
       <List style={{marginTop: 12, width: '100%'}} renderHeader={<View style={styles.modalCancel}>
-        <Text style={styles.modalCancelText}>开始时间</Text><Text style={styles.modalCancelText}>——</Text><Text style={styles.modalCancelText}>结束时间</Text>
+        <Text style={styles.modalCancelText}>开始时间</Text><Text style={styles.modalCancelText}>——</Text><Text
+        style={styles.modalCancelText}>结束时间</Text>
       </View>}/>
       <DatePickerView value={this.state.startNewDateValue} minDate={new Date(dealTargetDays)} maxDate={new Date()}
                       mode='date'
@@ -282,14 +301,14 @@ class DistributionanalysisScene extends PureComponent {
 
     let startNewDate = this.state.startNewDateValue
     let endNewDate = this.state.endNewDateValue
-    startNewDate = Math.round(new Date(new Date(startNewDate.setHours(0,0,0,0)).getTime()) / 1000);
-    endNewDate = Math.round(new Date(new Date(endNewDate.setHours(0,0,0,0)).getTime()+(24*60*60*1000-1)) / 1000);
+    startNewDate = Math.round(new Date(new Date(startNewDate.setHours(0, 0, 0, 0)).getTime()) / 1000);
+    endNewDate = Math.round(new Date(new Date(endNewDate.setHours(0, 0, 0, 0)).getTime() + (24 * 60 * 60 * 1000 - 1)) / 1000);
     this.getDistributionAnalysisData(startNewDate, endNewDate)
   }
 
   render() {
     return (
-        this.renderCellItem()
+      this.renderCellItem()
     );
   }
 }
