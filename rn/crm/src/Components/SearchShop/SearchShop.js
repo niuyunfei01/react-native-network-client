cb1e1468217b542cf4f86a04fc627772f749bc54
zhongyunyong123456 <95730651+zhongyunyong123456@users.noreply.github.com>
2021-12-23 16:03:34 +0800

    Revert "Revert "新建部分地图选择""
    This reverts commit 3d76f0650324f82483ea51cdc82de7e373d94b06.

--- rn/crm/src/Components/SearchShop/SearchShop.js
+++ rn/crm/src/Components/SearchShop/SearchShop.js
@@ -25,8 +25,6 @@ const RadioItem = Radio.RadioItem;
 
 
 class SearchShop extends Component {
-
-
     constructor(props) {
         super(props);
         const {limit_store, onBack, isType} = this.props.route.params;
@@ -209,6 +207,11 @@ class SearchShop extends Component {
                 </View>
                 {/*<ScrollView/>*/}
                 <WebView
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> parent of 79c3601c (Revert "Merge branch 'YF-2030' into beta")
                     source={{uri:'https://fire4.waisongbang.com/map.html'}}
                     onMessage={(event) => {
                         let cityData = JSON.parse(event.nativeEvent.data)
@@ -224,7 +227,7 @@ class SearchShop extends Component {
                         }
 
                     }}
-                    style={{display:'none'}}
+                    // style={{display:'none'}}
                 />
             </View>
 
