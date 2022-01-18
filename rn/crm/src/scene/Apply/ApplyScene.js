89412ccc4c4a1846f20843ddaf65d7c58cc4d410
zhongyunyong123456 <95730651+zhongyunyong123456@users.noreply.github.com>
2022-01-18 17:16:39 +0800

    去掉打印

--- rn/crm/src/scene/Apply/ApplyScene.js
+++ rn/crm/src/scene/Apply/ApplyScene.js
@@ -198,7 +198,6 @@ class ApplyScene extends PureComponent {
     doApply() {
         this.setState({doingApply: true});
         showModal("提交中")
-        console.log(this.state.pickerValue)
         let data = {
             sale_category: this.state.pickerValue,
             mobile: this.state.mobile,
@@ -215,7 +214,7 @@ class ApplyScene extends PureComponent {
 
         const {dispatch, navigation} = this.props;
         dispatch(customerApply(data, (success, msg, res) => {
-
+            this.doneApply();
             if (success) {
 
                 this.showSuccessToast(applySuccessMsg);
