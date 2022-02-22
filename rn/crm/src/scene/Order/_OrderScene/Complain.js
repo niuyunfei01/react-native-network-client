a509c045ce4bdfa1b7ac5039ba030297fa3c7435
zhongyunyong123456 <95730651+zhongyunyong123456@users.noreply.github.com>
2022-02-22 15:02:17 +0800

    前端样式备份

--- rn/crm/src/scene/Order/_OrderScene/Complain.js
+++ rn/crm/src/scene/Order/_OrderScene/Complain.js
@@ -10,6 +10,7 @@ import CallImg from "../CallImg";
 import native from "../../../common/native";
 import tool from "../../../common/tool";
 import {showError, ToastShort} from "../../../util/ToastUtils";
+import config from "../../../config";
 
 function mapStateToProps(state) {
   const {global} = state;
@@ -110,9 +111,13 @@ class Complain extends React.Component {
           }
           style={{backgroundColor: colors.main_back, flexGrow: 1}}
         >
+          <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate(config.ROUTE_COMPLAINDETAILS);
+          }}>
           <View style={{flexDirection: "row", alignItems: "center", justifyContent: "flex-start",backgroundColor:'gold',height:pxToDp(80)}}>
             <Text style={{marginLeft:pxToDp(40)}}>提示：请在投诉或索赔钱点击此处查看指引说明。</Text>
           </View>
+          </TouchableOpacity>
           <View style={{
             padding: '4%',
             backgroundColor: colors.white,
--- rn/crm/src/scene/Order/_OrderScene/ComplainDetails.js
+++ rn/crm/src/scene/Order/_OrderScene/ComplainDetails.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import {connect} from "react-redux";
 import HttpUtils from "../../../util/http";
 import {bindActionCreators} from "redux";
 import * as globalActions from "../../../reducers/global/globalActions";
-import {Clipboard, Platform, RefreshControl, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import {
+    Clipboard,
+    Image,
+    Platform,
+    RefreshControl,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from "react-native";
 import pxToDp from "../../../util/pxToDp";
 import colors from "../../../styles/colors";
 import CallImg from "../CallImg";
 import native from "../../../common/native";
 import tool from "../../../common/tool";
 import {showError, ToastShort} from "../../../util/ToastUtils";
+import {Cell, CellBody, CellFooter, Cells} from "../../../weui";
+import JbbText from "../../component/JbbText";
+import {List} from "@ant-design/react-native";
 
 function mapStateToProps(state) {
     const {global} = state;
@@ -23,36 +36,109 @@ function mapDispatchToProps(dispatch) {
         }, dispatch)
     }
 }
+class ImageBtn extends PureComponent {
+    constructor(props) {
+        super(props)
+    }
 
+    render() {
+        const {source, onPress, imageStyle, ...others} = this.props;
+        return <TouchableOpacity onPress={onPress} others>
+            <Image source={source} style={[styles.btn4text, {alignSelf: 'center', marginLeft: pxToDp(20)}, imageStyle]}/>
+        </TouchableOpacity>
+    }
+}
 class ComplainDetails extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            accessToken: this.props.global.accessToken,
-            delivery_id: this.props.route.params.id,
-            store_name: '',
-            store_id: '',
-            list: [],
-            content: '',
-            mobile: '',
+            changeHide:false,
             complain: []
         }
     }
 
 
-
-
-
-
     render() {
         return (
             <View>
-                <View>
-                    <Text>1111</Text>
+                <View style={{marginTop: 4}}>
+                    <Cells style={[styles.cell_box]}>
+                        <Cell customStyle={[styles.cell_row]} onPress={() => {
+                            this.changeHideFn()
+                        }}>
+                            <CellBody>
+                                <Text style={[styles.cell_body_text]}>如何正确申请投诉或索赔？</Text>
+                            </CellBody>
+                            <CellFooter>
+                                <ImageBtn source={
+                                    this.state.changeHide ? require('../../../img/Order/pull_up.png') : require('../../../img/Order/pull_down.png')
+                                }
+                                          imageStyle={styles.pullImg}
+                                />
+                            </CellFooter>
+                        </Cell>
+
+                        <If condition={this.state.changeHide}>
+                            {this.renderRrinter()}
+                        </If>
+                    </Cells>
                 </View>
             </View>
         )
     }
+    changeHideFn() {
+        this.setState({
+            changeHide:!this.state.changeHide
+        })
+    }
+    renderRrinter = () => {
+
+        return <View>
+            <Text>1111</Text>
+        </View>
+    }
 }
+
+const styles = StyleSheet.create({
+
+    cell_box: {
+        marginTop: 0,
+        borderTopWidth: pxToDp(1),
+        borderBottomWidth: pxToDp(1),
+        borderColor: colors.color999,
+    },
+    cell_row: {
+        height: pxToDp(70),
+        justifyContent: 'center',
+        paddingRight: pxToDp(10),
+    },
+    cell_input: {
+        //需要覆盖完整这4个元素
+        fontSize: 15,
+        height: 45,
+        marginTop: 2,
+        marginBottom: 2,
+    },
+    cell_body_text: {
+        fontSize: pxToDp(30),
+        fontWeight: 'bold',
+        color: colors.color333,
+    },
+    right_btn: {
+        fontSize: pxToDp(25),
+        paddingTop: pxToDp(8),
+        marginLeft: pxToDp(10),
+    },
+
+    image: {
+        width: '100%',
+        height: pxToDp(420),
+    },
+    btn_submit: {},
+    pullImg: {
+        width: pxToDp(90),
+        height: pxToDp(72)
+    },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ComplainDetails)
