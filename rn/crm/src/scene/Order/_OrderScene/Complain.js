f9c7adb6756682182c0e58fd8d88a24db04f2285
zhongyunyong123456 <95730651+zhongyunyong123456@users.noreply.github.com>
2022-02-23 17:10:52 +0800

    错别字

--- rn/crm/src/scene/Order/_OrderScene/Complain.js
+++ rn/crm/src/scene/Order/_OrderScene/Complain.js
@@ -115,7 +115,7 @@ class Complain extends React.Component {
             this.props.navigation.navigate(config.ROUTE_COMPLAINDETAILS);
           }}>
           <View style={{flexDirection: "row", alignItems: "center", justifyContent: "flex-start",backgroundColor:'gold',height:pxToDp(80)}}>
-            <Text style={{marginLeft:pxToDp(40)}}>提示：请在投诉或索赔钱点击此处查看指引说明。</Text>
+            <Text style={{marginLeft:pxToDp(40)}}>提示：请在投诉或索赔前点击此处查看指引说明。</Text>
           </View>
           </TouchableOpacity>
           <View style={{
