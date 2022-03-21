fc0f1638fbf4308a764487b3a924bc743f5bb0d2
zhongyunyong123456 <95730651+zhongyunyong123456@users.noreply.github.com>
2022-03-21 18:58:35 +0800

    样式

--- rn/crm/src/common/AppNavigator.js
+++ rn/crm/src/common/AppNavigator.js
@@ -57,6 +57,9 @@ const AppNavigator = (props) => {
         <Stack.Screen name="Order" options={{headerTitle: '订单详情'}}
                       getComponent={() => require("../scene/Order/OrderInfo").default}
                       initialParams={initialRouteParams}/>
+        <Stack.Screen name="OrderOperation" options={{headerTitle: '订单操作'}}
+                      getComponent={() => require("../scene/Order/OrderOperation").default}
+                      initialParams={initialRouteParams}/>
         <Stack.Screen name="Web" options={{headerShown: true}}
                       getComponent={() => require("../widget/WebScene").default}/>
         <Stack.Screen name="Home" getComponent={() => require("../scene/Remind/RemindScene").default}
