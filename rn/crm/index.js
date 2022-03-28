117deb92f3fdb3563713fd58cadcf5e3a155f2ff
hwcncd <dingzhigang@cainiaomeishi.com>
2022-03-28 14:46:12 +0800

    Revert "test"
    This reverts commit dd6ada35744155de1ccbcc3265d72e55565922d8.

--- rn/crm/index.js
+++ rn/crm/index.js
@@ -1,31 +1,7 @@
 import React, {Component} from 'react';
 import {AppRegistry, UIManager} from 'react-native';
 import RootScene from "./src/RootScene";
-const modules = require.getModules();
-const moduleIds = Object.keys(modules);
-const loadedModuleNames = moduleIds
-  .filter((moduleId) => modules[moduleId].isInitialized)
-  .map((moduleId) => modules[moduleId].verboseName);
-const waitingModuleNames = moduleIds
-  .filter((moduleId) => !modules[moduleId].isInitialized)
-  .map((moduleId) => modules[moduleId].verboseName);
 
-// make sure that the modules you expect to be waiting are actually waiting
-console.log(
-  'loaded:',
-  loadedModuleNames.length,
-  'waiting:',
-  waitingModuleNames.length
-);
-
-// grab this text blob, and put it in a file named packager/modulePaths.js
-console.log(
-  `module.exports = ${JSON.stringify(
-    loadedModuleNames.sort(),
-    null,
-    2
-  )};`
-);
 // 启用Android Layout动画
 UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
 
