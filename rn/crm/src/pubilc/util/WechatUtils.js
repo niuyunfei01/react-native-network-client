cd234cccfc3cd4a2a2c32dde3a16349adb5d0725
hwcncd <dingzhigang@cainiaomeishi.com>
2022-04-07 10:30:29 +0800

    增加分享微信朋友圈及对话

--- rn/crm/src/pubilc/util/WechatUtils.js
+++ rn/crm/src/pubilc/util/WechatUtils.js
@@ -7,7 +7,7 @@ import {getReadableVersion} from "react-native-device-info/src/index";
 
 wechat.registerApp(Config.APP_ID, Config.universalLink).then(r => console.log("register done:", r));
 
-export function JumpMiniProgram(path = "/pages/service/index", data = {}) {
+function JumpMiniProgram(path = "/pages/service/index", data = {}) {
   let str = '?ver=' + getReadableVersion() + '&';
   if (tool.length(data) > 0) {
     let paramsArray = [];
@@ -19,5 +19,18 @@ export function JumpMiniProgram(path = "/pages/service/index", data = {}) {
     path: path + str
   }, (res) => {
   });
-
 }
+
+// JumpWeb('测试','测试描述','https://cnsc-pics.cainiaoshicai.cn/platformLogo/1.png','https://www.waisongbang.com')
+function JumpWeb(title, desc, img, url, type = 0) {
+  wechat.shareWebpage({
+    title: title,
+    description: desc,
+    thumbImageUrl: img,
+    webpageUrl: url,
+    scene: type,
+  });
+}
+
+export {JumpMiniProgram, JumpWeb}
+
