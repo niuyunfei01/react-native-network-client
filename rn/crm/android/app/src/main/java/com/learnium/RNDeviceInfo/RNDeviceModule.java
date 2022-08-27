a1b14ac2132a2a680421ee18a09c8847f5df6a87
wangzhi17 <wangzhi0114@163.com>
2022-08-27 17:46:33 +0800

    1、修复GlobalCtx错误 2、修改发单界面的样式

--- rn/crm/android/app/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ rn/crm/android/app/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -14,6 +14,7 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
+import android.provider.Settings;
 import android.provider.Settings.Secure;
 import android.telephony.TelephonyManager;
 import android.text.format.Formatter;
