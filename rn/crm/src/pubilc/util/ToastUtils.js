506c04cf8793036f3c647aec63e70b4955aacd59
hwcncd <dingzhigang@cainiaomeishi.com>
2022-12-26 11:56:11 +0800

    fix-bug

--- rn/crm/src/pubilc/util/ToastUtils.js
+++ rn/crm/src/pubilc/util/ToastUtils.js
@@ -3,38 +3,49 @@ import React from 'react';
 import Toast from "react-native-root-toast";
 import {ActivityIndicator, StyleSheet, Text, View} from 'react-native'
 import {Icon} from "../../weui/Icon";
+import tool from "./tool";
 
 let show_toast = null;
 
 export function ToastShort(content, position = -100) {
   Toast.hide(show_toast)
-  show_toast = Toast.show(content, {
-    duration: Toast.durations.SHORT,
-    position: position,
-    shadow: true,
-    animation: true,
-    hideOnPress: true,
-    delay: 0,
-  });
+  if (tool.length(content) > 0) {
+    show_toast = Toast.show(content, {
+      duration: Toast.durations.SHORT,
+      position: position,
+      shadow: true,
+      animation: true,
+      hideOnPress: true,
+      delay: 0,
+    });
+  }
 }
 
 export function ToastLong(content, position = -100) {
   Toast.hide(show_toast)
-  show_toast = Toast.show(content, {
-    duration: Toast.durations.LONG,
-    position: position,
-    shadow: true,
-    animation: true,
-    hideOnPress: true,
-    delay: 0,
-  });
+  if (tool.length(content) > 0) {
+    show_toast = Toast.show(content, {
+      duration: Toast.durations.LONG,
+      position: position,
+      shadow: true,
+      animation: true,
+      hideOnPress: true,
+      delay: 0,
+    });
+  }
 }
 
 export function showModal(content, icon = 'loading', timeOut = 6000, position = 0) {
 
   Toast.hide(show_toast)
   show_toast = Toast.show(
-    <View style={{alignItems: 'center', justifyContent: 'center', minWidth: 150, paddingVertical: 10}}>
+    <View style={{
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: 150,
+      paddingVertical: 10,
+      paddingHorizontal: 20
+    }}>
       {icon === 'loading' ? <ActivityIndicator color='#fff' size={'large'}/> :
         <Icon name={icon} style={[styles.toastIcon]}/>}
       <Text style={{color: '#ffffff', fontSize: 22, marginTop: 12}}> {content} </Text>
@@ -55,7 +66,13 @@ export function hideModal() {
 export function showSuccess(content, position = 0) {
   Toast.hide(show_toast)
   show_toast = Toast.show(
-    <View style={{alignItems: 'center', justifyContent: 'center', minWidth: 150, paddingVertical: 10}}>
+    <View style={{
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: 150,
+      paddingVertical: 10,
+      paddingHorizontal: 20
+    }}>
       <Icon name={'success'} style={[styles.toastIcon]}/>
       <Text style={{color: '#ffffff', fontSize: 22, marginTop: 12}}> {content} </Text>
     </View>, {
@@ -71,7 +88,13 @@ export function showSuccess(content, position = 0) {
 export function showError(content, position = 0) {
   Toast.hide(show_toast)
   show_toast = Toast.show(
-    <View style={{alignItems: 'center', justifyContent: 'center', minWidth: 150, paddingVertical: 10}}>
+    <View style={{
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: 150,
+      paddingVertical: 10,
+      paddingHorizontal: 20
+    }}>
       <Icon name={'warn'} style={[styles.toastIcon]}/>
       <Text style={{color: '#ffffff', fontSize: 22, marginTop: 12}}> {content} </Text>
     </View>, {
